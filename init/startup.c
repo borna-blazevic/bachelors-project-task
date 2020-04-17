ec2f129870d0b24e39bf2c86c625281aeff3ad2f
borna-blazevic <borna.blazevic@fer.hr>
2020-04-17 12:49:34 +0200

    set uart handler to uart0

--- init/startup.c
+++ init/startup.c
@@ -88,8 +88,8 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,								// GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
-    IntDefaultHandler,						// UART0 Rx and Tx
-    vUART_ISR,                              // UART1 Rx and Tx
+    vUART_ISR,						        // UART0 Rx and Tx
+    IntDefaultHandler,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI Rx and Tx
     IntDefaultHandler,                      // I2C Master and Slave
     IntDefaultHandler,                      // PWM Fault
