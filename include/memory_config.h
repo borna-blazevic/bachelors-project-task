575b3d25210c27554250633ce81b907368dab032
borna-blazevic <borna.blazevic@fer.hr>
2020-05-20 21:42:40 +0200

    fix issues with firmware upgrade

--- include/memory_config.h
+++ include/memory_config.h
@@ -1,2 +1,4 @@
-extern uint32_t _shared_data_start;
-extern uint8_t _app_start2;
+extern uint32_t _shared;
+extern uint8_t _firmware_upgrade;
+extern uint32_t _new_image;
+extern uint32_t _recovery_image;
--- main.c
+++ main.c
@@ -14,8 +14,8 @@ int main(void)
 	FirmwareUpgrade__SrecLine *srec_line;
 	uint8_t packet_size;
 	uint8_t message_buffer[MAX_MSG_SIZE];
-	uint8_t *pImage = &_app_start2;
-	uint32_t *bootenv = &_shared_data_start;
+	uint8_t *pImage = &_firmware_upgrade;
+	uint32_t *bootenv = &_shared;
 
 	NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x20000);
 	prvSetupHardware();
@@ -29,7 +29,7 @@ int main(void)
 		NVIC_SystemReset();
 	}
 	print_string("booted task and upgrading\n");
-
+	bootenv[55] = 0;
 	while (1)
 	{
 		print_string("hi\n");
@@ -43,15 +43,19 @@ int main(void)
 			continue;
 		}
 
-		if (!strcmp(srec_line->record_type, "S5"))
+		if (!strcmp(srec_line->record_type, "S5")){
+			print_string("Got S5\n");
 			break;
+		}
 		if (!strcmp(srec_line->record_type, "S0"))
 			continue;
 
-		memcpy(&pImage[srec_line->address/4], srec_line->data.data, srec_line->data.len);
+		memcpy(&pImage[srec_line->address], srec_line->data.data, srec_line->data.len);
+		bootenv[50] = (uint32_t) &pImage[srec_line->address + srec_line->data.len];
   		firmware_upgrade__srec_line__free_unpacked(srec_line, NULL);
 	}
-	bootenv[50] = (uint32_t) &pImage[srec_line->address/4 + srec_line->data.len];
+	bootenv[55] = 1;
+	*bootenv = 2;
 
 	NVIC_SystemReset();
 }
